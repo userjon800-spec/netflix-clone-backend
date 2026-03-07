46d958039b503ff93e1f060264cb757cf5f48725
Javohir <userjon800@gmail.com>
2026-03-07 22:38:14 +0500

    Kun tugadi

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model");
 const bcrypt = require("bcrypt");
+const movieModel = require("../models/movie.model");
 class UserController {
   async getUser(req, res) {
     try {
@@ -38,7 +39,27 @@ class UserController {
         { password: hashedPass },
         { new: true },
       );
-      res.status(202).json({message: "Load new password"})
+      res.status(202).json({ message: "Load new password" });
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: "Xatolik iltimos keyinroq urining" });
+    }
+  }
+  async likedMovie(req, res) {
+    try {
+      await movieModel.create(req.body);
+      res.status(200).json({message: 'Succes'})
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ message: "Xatolik iltimos keyinroq urining" });
+    }
+  }
+  async unLikedMovie(req, res) {
+    try {
+      const {id}=req.params
+      const unLike = await movieModel.find({id}).lean()
+      await movieModel.findByIdAndDelete(unLike)
+      res.status(200).json({message: 'UnLike succes'})
     } catch (error) {
       console.error(error);
       res.status(401).json({ message: "Xatolik iltimos keyinroq urining" });
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,8 @@
 const userController = require("../controllers/user.controller");
 const router = require("express").Router();
-router.get('/user/:id',userController.getUser)
-router.put('/user-update',userController.updateUser)
-router.put('/user-pass',userController.updatePassword)
+router.get("/user/:id", userController.getUser);
+router.put("/user-update", userController.updateUser);
+router.put("/user-pass", userController.updatePassword);
+router.post("/liked-movie", userController.likedMovie);
+router.delete("/un-liked-movie/:id", userController.unLikedMovie);
 module.exports = router;
